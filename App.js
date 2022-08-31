96c3b2f2ab9e5ac06d240d0418a72c4ff6220337
GUEST-1001 <nawatgame@live.com>
2022-08-31 09:10:48 +0700

    Recap 'Stack Navigator' 31 08 2022

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
-import FirstPage from './screens/FirstPage';
-import SecondPage from './screens/SecondPage';
+// import FirstPage from './screens/FirstPage';
+// import SecondPage from './screens/SecondPage';
+import FirstPage from './pages/FirstPage';
+import SecondPage from './pages/SecondPage';
+import ThirdPage from './pages/ThirdPage';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,13 +19,14 @@ export default function App() {
       <Stack.Navigator
         initialRouteName='Home'
         screenOptions={{
-          headerStyle: { backgroundColor: 'blue' },
+          headerStyle: { backgroundColor: '#3EB489' },
           headerTintColor: '#ffff',
           headerTitleStyle: { fontWeight: 'bold' }
         }}
       >
         <Stack.Screen name='First Page' component={FirstPage} />
         <Stack.Screen name='Second Page' component={SecondPage} />
+        <Stack.Screen name='Third Page' component={ThirdPage} />
       </Stack.Navigator>
     </NavigationContainer>
 
