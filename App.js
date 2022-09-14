93b70fd2966fabd1edd7301c5f7b1cf4ff031c25
GUEST-1001 <nawatgame@live.com>
2022-09-14 09:46:55 +0700

    14 09 2022 "Practice Tab and Drawer Navigator"

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
 
+import { Ionicons } from "@expo/vector-icons";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 function HomeScreen({ navigation }) {
   return (
@@ -25,20 +28,47 @@ function SettingScreen({ navigation }) {
 }
 
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 function MyTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Home") {
+            iconName = focused
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
+          } else if (route.name === "Setting") {
+            iconName = focused ? "ios-list-box" : "ios-list";
+          }
+          //can return any component
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Setting" component={SettingScreen} />
     </Tab.Navigator>
   );
 }
 
+function MyDrawer() {
+  return (
+    <Drawer.Navigator useLegacyImplementation>
+      <Drawer.Screen name="Home" component={MyTab} />
+      <Drawer.Screen name="Setting" component={SettingScreen} />
+    </Drawer.Navigator>
+  );
+}
+
 const App = () => {
   return (
     <NavigationContainer>
-      <MyTab />
+      <MyDrawer />
     </NavigationContainer>
   );
 };
